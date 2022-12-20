42f7b1dc8a36e3897bf593c66f7678dec068ed4b
ProspectPyxis <ProspectPyxis@users.noreply.github.com>
2022-12-20 16:41:21 +0700

    feat: database file generation now works properly

--- functions/__omnicow_db_generate.fish
+++ functions/__omnicow_db_generate.fish
@@ -8,6 +8,47 @@ function __omnicow_db_generate
         __omnicow_db_generate_help
         return 0
     end
+
+    set --query COWPATH; or set -l COWPATH /usr/share/cows
+    set -l dbfile $omnicow_omnicowdb_path/$omnicow_omnicowdb_filename
+    if test -e $dbfile; and not set --query _flag_force; and test (__omnicow_db_get all_files) = (string join ' ' $COWPATH/*.cow)
+        return 0
+    end
+
+    set -l min_width
+    set -l max_width
+    set -l min_height
+    set -l max_height
+    set -l first_file_read false
+    printf "allfiles,%s\n" (string join ',' $COWPATH/*.cow) >$dbfile
+    for cowfile in $COWPATH/*.cow
+        set -l cowfile_md5 (md5sum $cowfile | awk '{print $1}')
+        set -l parsed_cow (awk '/^\$the_cow.*EOC/{flag=1; next} /^EOC$/{flag=0} flag' $cowfile \
+            | string replace --all '\'' '\\\'' | string replace --all '"' '\\"' | string unescape \
+            | string replace --all '$thoughts' '\\' | string replace --all '$tongue' '  ')
+        # TODO: Parse cowfiles with eye width != 2
+        set parsed_cow (string replace --all --regex '\$eyes|\$\{eyes\}' 'oo' $parsed_cow)
+
+        set -l width (printf "%s\n" $parsed_cow | wc -L)
+        set -l height (printf "%s\n" $parsed_cow | wc -l)
+        if not $first_file_read
+            set first_file_read true
+            set min_width $width
+            set max_width $width
+            set min_height $height
+            set max_height $height
+        else
+            set min_width (math min $width,$min_width)
+            set max_width (math max $width,$max_width)
+            set min_height (math min $height,$min_height)
+            set max_height (math max $height,$max_height)
+        end
+        printf "cowfile,%s,%s,%d,%d\n" $cowfile $cowfile_md5 $width $height >>$dbfile
+    end
+    printf "value,min_width,%d\n" $min_width >>$dbfile
+    printf "value,max_width,%d\n" $max_width >>$dbfile
+    printf "value,min_height,%d\n" $min_height >>$dbfile
+    printf "value,max_height,%d\n" $max_height >>$dbfile
 end
 
 function __omnicow_db_generate_help
