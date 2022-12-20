c6cf98d2a0a4e0452cba3981d7e2dca63ca3b556
ProspectPyxis <ProspectPyxis@users.noreply.github.com>
2022-12-20 14:11:59 +0700

    feat: framework of db generate function

---
+++ functions/__omnicow_db_generate.fish
@@ -0,0 +1,21 @@
+function __omnicow_db_generate
+    if not argparse --name="omnicow db generate" h/help f/force -- $argv
+        __omnicow_db_generate_help
+        return 1
+    end
+    if set --query $_flag_help
+        __omnicow_db_generate_help
+        return 0
+    end
+end
+
+function __omnicow_db_generate_help
+    printf %s\n \
+        'omnicow db generate: Generate or regenerate cowfile database' \
+        '' \
+        'Usage: omnicow db generate [OPTIONS]' \
+        '' \
+        'Options:' \
+        '  -f, --force  Force regenerate the database, even if no files have been added or removed' \
+        '  -h, --help   Print help information'
+end
