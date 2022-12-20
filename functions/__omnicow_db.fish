6988a35deb3b285a7bc5198fc7d338b6189100d5
ProspectPyxis <ProspectPyxis@users.noreply.github.com>
2022-12-20 14:04:32 +0700

    feat: omnicow db function

---
+++ functions/__omnicow_db.fish
@@ -0,0 +1,31 @@
+function __omnicow_db
+    argparse --stop-nonopt h/help -- $argv
+    if set --query _flag_help
+        __omnicow_db_help
+    else if not set --query $argv[1]
+        __omnicow_db_help
+        return 1
+    else if test $argv[1] = generate -o $argv[1] = gen
+        __omnicow_db_generate
+    else if functions --query __omnicow_db_$argv[1]
+        __omnicow_db_$argv[1] $argv[2..]
+    else
+        __omnicow_db_help
+        return 1
+    end
+end
+
+function __omnicow_db_help
+    printf %s\n \
+        'omnicow db: Manage omnicow database' \
+        '' \
+        'Usage: omnicow db [OPTIONS] [SUBCOMMAND]' \
+        '' \
+        'Options:' \
+        '  -h, --help  Print help information' \
+        '' \
+        'Subcommands:' \
+        '  gen, generate  Generate or regenerate cowfile database' \
+        '  rm             Delete cowfile database' \
+        '  get            Query the cowfile database'
+end
