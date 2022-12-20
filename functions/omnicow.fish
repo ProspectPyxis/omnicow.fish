d4e6819706ad0f276efe6f31c1f80c74bc7d2548
ProspectPyxis <ProspectPyxis@users.noreply.github.com>
2022-12-20 20:16:51 +0700

    docs: improved help output

--- functions/omnicow.fish
+++ functions/omnicow.fish
@@ -1,5 +1,5 @@
 function omnicow
-    argparse --stop-nonopt h/help -- $argv
+    argparse --stop-nonopt --ignore-unknown h/help -- $argv 2>/dev/null
     if set --query _flag_help
         __omnicow_help
     else if functions --query __omnicow_$argv[1]
@@ -16,7 +16,12 @@ function __omnicow_help
         '  omnicow [SUBCOMMAND]' \
         '' \
         'Options (all options from cowsay are also valid):' \
-        '  -h, --help  Print help information' \
+        '  --say, --think             Forces the usage of cowsay or cowthink, respectively, rather than picking randomly. These options are mutually exclusive.' \
+        '  -E, --default-eyes         Always use the default eyes rather than randomizing.' \
+        '  -A, --no-exclude           Do not exclude cowfiles listed in $omnicow_exclude_cowfiles from being picked.' \
+        '  --max-width <MAX_WIDTH>    The maximum width of the picked cow.' \
+        '  --max-height <MAX_HEIGHT>  The maximum height of the picked cow.' \
+        '  -h, --help                 Print help information' \
         '' \
         'Subcommands:' \
         '  db  Manage the omnicow database'
